82b4f2c22281693dbb858c8e1b3d727450731620
Rafael Câmara <rafaelcamaram@gmail.com>
2022-06-07 09:39:28 +0100

    feat: Creating script to interact with smart contract for the first time

---
+++ scripts/interact.js
@@ -0,0 +1,22 @@
+const { ethers } = require("ethers");
+const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+
+const { API_KEY, PRIVATE_KEY, CONTRACT_ADDRESS } = process.env;
+
+const alchemyProvider = new ethers.providers.AlchemyProvider('goerli', API_KEY);
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main() {
+    const message = await helloWorldContract.message();
+    console.log(`The message is: ${message}`);
+
+    console.log('Updating the message...')
+    const tx = await helloWorldContract.update("This is the new message.")
+    await tx.wait();
+
+    const newMessage = await helloWorldContract.message();
+    console.log(`The new message is: ${newMessage}`);
+}
+
+main();
